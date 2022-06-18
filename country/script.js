37a1eb0c4c01b23eaa541dc6f497a28a9337615b
zephyr <zroumane@gmail.com>
2022-06-18 19:31:46 +0200

    fix: country

--- country/script.js
+++ country/script.js
@@ -2,12 +2,13 @@ import fetch from 'node-fetch'
 import {writeFileSync} from 'fs'
 
 
-let continents = {'Africa': [], 'Americas': [], 'Asia': [], 'Europe': []}
+let continents = {'World': [],'Africa': [], 'Americas': [], 'Asia': [], 'Europe': []}
 let countries = []
 let flags = new Map()
 let n = 0
 
 for (const continent in continents) {
+  if(continent == 'World') continue
   continents[continent].push(n)
   let response = await fetch(`https://restcountries.com/v2/region/${continent}?fields=alpha2Code,name,flag`)
   let data = await response.json()
@@ -19,6 +20,8 @@ for (const continent in continents) {
   n += data.length 
   continents[continent].push(n - 1)
 }
+
+continents['World'] = [0, n]
 
 writeFileSync("./server/continents.json", JSON.stringify(continents))
 writeFileSync("./server/countries.json", JSON.stringify(countries))
